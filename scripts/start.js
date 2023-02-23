42eafb61f82ef715ecb2f266db6b7ca7b9f58d41
Falcon <annfalcon@gmail.com>
2023-02-23 00:09:31 +0000

    adds books from api

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,30 @@
-$(document).ready(()=>{
-    
-})
+$(document).ready(() => {});
+
+async function getBooks(kind) {
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${kind}`
+  );
+  if (response.status == 200) {
+    const data = await response.json();
+    let books = data.items;
+
+    console.log(data);
+
+    for (let i = 0; i < books.length; i++) {
+      $("#books-container").append(`<div class="book">
+        <img src="${books[i].volumeInfo.imageLinks.thumbnail}" alt="" srcset="">
+        <div class="title">
+        ${books[i].volumeInfo.title}
+        </div>
+        <div class="desc">
+        ${books[i].volumeInfo.authors || ""}
+        </div>
+        
+      </div>`);
+    }
+  }
+}
+
+getBooks("novels");
+getBooks("fiction");
+getBooks("non-fiction");
