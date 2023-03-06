5f20c817ca823be5b30cbf9614f760373841a9d4
Falcon <annfalcon@gmail.com>
2023-03-06 00:52:09 +0000

    Adds fetch bestsellers from New York Times

--- scripts/start.js
+++ scripts/start.js
@@ -1,17 +1,34 @@
 $(document).ready(() => {
   $(".category").click(function () {
     const category = $(this).text();
+    $("#books-container").empty();
 
-  $(".category").removeClass("selected")
-
-  $(this).addClass("selected")
-
+    $(".category").removeClass("selected");
+    $(this).addClass("selected");
     getBooks(category);
+  });
+
+  $(".all-category").click(function() {
+      $(".category").removeClass("selected")
+      $("#books-container").empty();
+      $(this).addClass("selected");
+      getBooks("novels");
+      getBooks("fiction");
+      getBooks("non-fiction");
+  })
+
+  $(".bestsellers").click(function () {
+    $(".category").removeClass("selected");
+    $("#books-container").empty();
+    
+    getBestSellers();
   });
 
   $(".search.btn").click(function () {
     const searchBox = $(".search-box").val();
+    $("#books-container").empty();
 
+    $(".category").removeClass("selected");
     getBooks(searchBox);
   });
 });
@@ -22,14 +39,13 @@ async function getBooks(kind) {
   );
   if (response.status == 200) {
     const data = await response.json();
-    let books = data.items;
+    let books = data.items || [];
 
     console.log(data);
-    $("#books-container").empty();
     for (let i = 0; i < books.length; i++) {
-      let thumbnail = ""
+      let thumbnail = "";
       if (books[i].volumeInfo.imageLinks) {
-        thumbnail=books[i].volumeInfo.imageLinks.thumbnail
+        thumbnail = books[i].volumeInfo.imageLinks.thumbnail;
       }
 
       $("#books-container").append(`<div class="book">
@@ -49,3 +65,25 @@ async function getBooks(kind) {
 getBooks("novels");
 getBooks("fiction");
 getBooks("non-fiction");
+
+
+async function getBestSellers(){
+  const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=XdeEe3wf6GBydft9tgGFNHyBjVRDK1jZ`)
+
+  if (response.status == 200) {
+    const data = await response.json();
+    let books = data.results;
+
+    console.log(books)
+    books.forEach(element => {
+      if (element.isbns.length){
+        const isbn=element.isbns[0].isbn13
+        getBooks(`isbn:${isbn}`)
+      }
+     
+      
+    });
+  }
+
+
+}
