595753ecc923fb49541cc37fa8bc5af76253ba98
Falcon <annfalcon@gmail.com>
2023-03-12 23:56:29 +0100

    Adds local storage-cart

--- scripts/start.js
+++ scripts/start.js
@@ -31,6 +31,35 @@ $(document).ready(() => {
     $(".category").removeClass("selected");
     getBooks(searchBox);
   });
+
+  $("#books-container").on("click", ".btn-add-cart", function (){
+    const item = {
+      title: $(this).data("title"),
+      price: parseInt($(this).data("price")),
+      quantity: parseInt($(this).data("quantity")),
+      image: $(this).data("image"),
+    }
+    console.log(item)
+    const id = $(this).data("id")
+
+    localStorage.setItem(id, JSON.stringify(item))
+  })
+
+  $(".basket-big").click(function(){
+    $("#offcanvas-cart").empty()
+
+    for (let i = 0; i<localStorage.length; i++) {
+      const key = localStorage.key(i)
+      const item = JSON.parse(localStorage.getItem(key))
+      console.log(item)
+      $("#offcanvas-cart").append(`<tr>
+        <td><img class="cart-img" src="${item.image}" alt="${item.title}" srcset=""></td>
+        <td>${item.quantity} </td>
+        <td class="cart-price">${item.price} &euro;</td>
+      </tr>`)
+    }
+  })
+
 });
 
 async function getBooks(kind) {
@@ -48,6 +77,8 @@ async function getBooks(kind) {
         thumbnail = books[i].volumeInfo.imageLinks.thumbnail;
       }
 
+    // Add book html to container
+
       $("#books-container").append(`<div class="book">
         <img class="imgBook" src="${thumbnail}" alt="" srcset="">
         <div class="title">
@@ -56,6 +87,8 @@ async function getBooks(kind) {
         <div class="desc">
         ${books[i].volumeInfo.authors || ""}
         </div>
+
+        <button type="button" class="btn btn-primary btn-sm btn-add-cart" data-id="${books[i].id}" data-title="${books[i].volumeInfo.title.replace(/\"/, '')}" data-image="${thumbnail}" data-price="10" data-quantity="1">Add to cart</button>
         
       </div>`);
     }
